e33502c7ffbb0949626751efcc6bffbeb5171b38
andrewlowell <andrewlowellr@gmail.com>
2019-02-22 10:50:59 -0700

    Finish carousel stretch and remove warnings.

--- lambdatimes/src/components/Carousel/Carousel.js
+++ lambdatimes/src/components/Carousel/Carousel.js
@@ -5,29 +5,50 @@ export default class Carousel extends Component {
   constructor(props){
     super(props);
     this.state = {
-
+      selectedImage: 0,
+      images: []
     }
   }
   componentDidMount(){
+    this.setState({images: carouselData})
+  }
 
+  returnNextLeft = current => {
+    switch(current) {
+      case 0:
+        return this.state.images.length - 1;
+      default:
+        return (current - 1);
+    }
+  }
+
+  returnNextRight = current => {
+    switch(current) {
+      case (this.state.images.length - 1):
+        return 0;
+      default:
+        return (current + 1);
+    }
   }
 
   leftClick = () => {
-
+    this.setState({selectedImage: this.returnNextLeft(this.state.selectedImage)});
   }
 
   rightClick = () => {
-
+    this.setState({selectedImage: this.returnNextRight(this.state.selectedImage)});
   }
 
   selectedImage = () => {
-    return <img src={} style={{display: 'block'}} />
+    console.log(this.state.images[this.state.selectedImage]);
+    return <img src={this.state.images[this.state.selectedImage]} style={{display: 'block'}} alt="remove warning" />
   }
   
   render(){
     return (
       <div className="carousel">
         <div className="left-button" onClick={this.leftClick}>{"<"}</div>
+        {this.selectedImage()}
         <div className="right-button" onClick={this.rightClick}>{">"}</div>
       </div>
     )
